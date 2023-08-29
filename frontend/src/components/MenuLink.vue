97644b664588f833c07458f9a58fe819acc074bf
ToKruAmApp <tobi.k1696@googlemail.com>
2023-08-29 22:32:18 +0200

    Add main menu

--- frontend/src/components/MenuLink.vue
+++ frontend/src/components/MenuLink.vue
@@ -23,7 +23,7 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'EssentialLink',
+  name: 'MenuLink',
   props: {
     title: {
       type: String,
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -2,7 +2,22 @@
   <q-layout view="lHh lpR lFf">
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" elevated>
-      <!-- drawer content -->
+      <div class="relative-position q-my-lg layout-desktop-logo">
+        <router-link to="/">
+          <img
+            src="/icons/favicon-128x128.png"
+            class="absolute-center layout-logo-img"
+          >
+        </router-link>
+      </div>
+      <q-list>
+        <MenuLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+          :entity="link.entity"
+        />
+      </q-list>
     </q-drawer>
 
     <q-page-container>
@@ -13,18 +28,66 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { defineComponent } from "vue"
+import MenuLink from "components/MenuLink.vue"
 
-export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
+const linksList = [
+  {
+    title: "Dashboard",
+    icon: "mdi-home",
+    link: "/",
+  },
+  {
+    title: "Kalender",
+    icon: "mdi-calendar",
+    link: "/calendar",
+  },
+  {
+    title: "Terminliste",
+    icon: "mdi-format-list-bulleted",
+    link: "/appointments",
+  },
+  {
+    title: "Kontakte",
+    icon: "mdi-account-group",
+    link: "/contacts",
+  },
+  {
+    title: "Orte",
+    icon: "mdi-map-marker-radius",
+    link: "/locations",
+  },
+  {
+    title: "Kategorien",
+    icon: "mdi-palette",
+    link: "/automated_forms",
+  },
+]
 
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    MenuLink,
+  },
+
+  data () {
     return {
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      essentialLinks: linksList,
+      leftDrawerOpen: false,
     }
-  }
-}
+  },
+
+  methods: {
+    toggleLeftDrawer () {
+      this.leftDrawerOpen = !this.leftDrawerOpen
+    },
+  },
+})
 </script>
+
+<style lang="scss">
+  .layout-desktop-logo {
+      height: 140px;
+  }
+</style>
