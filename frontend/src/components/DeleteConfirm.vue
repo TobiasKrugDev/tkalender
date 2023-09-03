82204ddedbd552f4d894caca3f164b0e4c2c9c60
ToKruAmApp <tobi.k1696@googlemail.com>
2023-09-04 00:28:50 +0200

    Add DeleteConfrim component

---
+++ frontend/src/components/DeleteConfirm.vue
@@ -0,0 +1,87 @@
+<template>
+    <q-card>
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">Löschen?</div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-section>
+          <div>
+            Sicher, dass Sie 
+            <span v-if="entity === 'appointment'">den Termin</span>
+            <span v-if="entity === 'contact'">den Kontakt</span>
+            <span v-if="entity === 'location'">den Ort</span>
+            <span v-if="entity === 'category'">die Kategorie</span>
+            "{{ itemName }}" wirklich löschen möchten?
+          </div>
+        </q-card-section>
+
+        <q-card-actions align="right">
+            <q-btn
+            v-close-popup
+            flat
+            label="Abbrechen"
+            color="primary"
+            />
+            <q-btn
+            label="Löschen"
+            color="negative"
+            @click="deleteItem"
+            />
+      </q-card-actions>
+    </q-card>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { mapActions } from "vuex"
+
+export default defineComponent({
+    name: 'DeleteConfirm',
+
+    props: {
+        item: {
+            type: Object,
+            required: true,
+            default: null,
+        },
+
+        entity: {
+            type: String,
+            required: true,
+            default: "",
+        },
+    },
+
+    computed: {
+        itemName () {
+            if (this.entity === "contact") {
+                return `${this.item.firstname} ${this.item.lastname}`
+            } else {
+                return this.item.name
+            }
+        },
+    },
+
+  methods: {
+    ...mapActions("appointments", ["deleteAppointment"]),
+    ...mapActions("contacts", ["deleteContact"]),
+    ...mapActions("locations", ["deleteLocation"]),
+    ...mapActions("categories", ["deleteCategory"]),
+
+
+    deleteItem () {
+        switch (this.entity) {
+            case "appointment": this.deleteAppointment(this.item.id); break;
+            case "contact": this.deleteContact(this.item.id); break;
+            case "location": this.deleteLocation(this.item.id); break;
+            case "category": this.deleteCategory(this.item.id); break;
+        }
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+</style>
