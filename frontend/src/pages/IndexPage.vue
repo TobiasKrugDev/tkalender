b153e1fb5747ee602be19d7477105f2893fbadb1
ToKruAmApp <tobi.k1696@googlemail.com>
2023-09-06 00:27:54 +0200

    Add customized floating action button

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page class="flex flex-center">
     ToDo: Dashboard
+
+    <q-page-sticky position="bottom-right" :offset="[75, 75]">
+      <q-btn fab icon="add" color="primary" size="xl" class="custom-floating-action-button" />
+    </q-page-sticky>
   </q-page>
 </template>
 
@@ -11,3 +15,15 @@ export default defineComponent({
   name: 'IndexPage',
 })
 </script>
+
+<style lang="scss">
+  .custom-floating-action-button {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+
+  .custom-floating-action-button .q-icon {
+    font-size: 40px;
+  }
+</style>
