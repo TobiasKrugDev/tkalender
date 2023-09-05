b566afd6bdcf85647ced33d21393db28ab30a815
ToKruAmApp <tobi.k1696@googlemail.com>
2023-09-05 21:24:47 +0200

    Add search bar

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,6 +1,33 @@
 <template>
   <q-layout view="lHh lpR lFf">
 
+    <q-header
+      class="bg-transparent row q-py-lg"
+      :class="$q.screen.gt.xs ? 'q-px-lg q-gutter-x-lg' : 'q-px-sm q-gutter-x-sm'"
+      style="flex-wrap: nowrap;"
+    >
+      <div class="blurred-background" />
+      <q-toolbar
+        class="bg-white shadow-7 rounded-borders"
+        :class="$q.screen.gt.xs ? 'q-px-lg' : 'q-px-sm'"
+        style="color: black;"
+      >
+        <q-toolbar-title>
+          <q-input
+            v-model="searchInput"
+            placeholder="Suche nach..."
+            borderless
+            type="search"
+            @keyup.enter="navigateToSearch"
+          >
+            <template #prepend>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left">
       <div class="relative-position q-my-lg layout-desktop-logo">
         <router-link to="/">
@@ -20,7 +47,7 @@
       </q-list>
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container class="page-container-background">
       <router-view />
     </q-page-container>
 
@@ -98,5 +125,13 @@ export default defineComponent({
     border-radius: 8px;
     height: calc(100% - 48px) !important;
     box-shadow: 0 4px 5px -2px rgba(0, 0, 0, 0.2), 0 7px 10px 1px rgba(0, 0, 0, 0.14), 0 2px 16px 1px rgba(0, 0, 0, 0.12)
+  }
+
+  .rounded-borders {
+    border-radius: 8px;
+  }
+
+  .page-container-background {
+    background-color: #f5f6fa;
   }
 </style>
