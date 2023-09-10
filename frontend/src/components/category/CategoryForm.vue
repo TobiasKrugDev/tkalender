fcf7b6134c794b2f5289f7647e22f348be825580
ToKruAmApp <tobi.k1696@googlemail.com>
2023-09-10 23:57:20 +0200

    Add CategoryForm component

---
+++ frontend/src/components/category/CategoryForm.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="q-mb-md">
+        <q-input 
+            ref="categoryNameInput"
+            v-model="category.name" 
+            outlined 
+            label="Name" 
+            :rules="[val => !!val || 'Name ist ein Pflichtfeld']"
+        />
+    </div>
+    <div class="q-mb-md">
+        <q-input
+            v-model="category.description"
+            label="Beschreibung"
+            outlined
+            type="textarea"
+        />
+    </div>
+    <div>
+        <q-color v-model="category.color" no-header-tabs no-footer class="my-picker q-mx-auto" />
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'CategoryForm',
+
+    data () {
+        return {
+            category: {}
+        }
+    },
+
+    methods: {
+        validateCategoryForm () {
+            // ToDo: Color Picker Validation
+            this.$refs.categoryNameInput.validate()
+
+            if (this.$refs.categoryNameInput.hasError) {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+</style>
