10dc795d8f9821cebcffaf7a35bff58b5574483c
ToKruAmApp <tobi.k1696@googlemail.com>
2023-09-23 01:50:35 +0200

    Add Google Maps Embed component

---
+++ frontend/src/components/GoogleMapsEmbed.vue
@@ -0,0 +1,41 @@
+<template>
+  <iframe
+    :src="
+      'https://maps.google.com/maps?&amp;q=' +
+      location.streetAddress +
+      ' ' +
+      location.postalCode +
+      ' ' +
+      location.city +
+      '&amp;output=embed'
+    "
+    width="500"
+    height="400"
+    loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"
+    class="google-maps-iframe"
+  ></iframe>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "GoogleMapsEmbed",
+
+  props: {
+    location: {
+      type: Object,
+      default: null,
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+.google-maps-iframe {
+  border: 0;
+  width: 100%;
+  border-radius: 8px;
+}
+</style>
