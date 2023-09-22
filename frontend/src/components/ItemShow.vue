46dddba134924ea1dc210765d828087af6535e0f
ToKruAmApp <tobi.k1696@googlemail.com>
2023-09-23 01:53:33 +0200

    Display appointment item information

--- frontend/src/components/ItemShow.vue
+++ frontend/src/components/ItemShow.vue
@@ -1,4 +1,70 @@
 <template>
+  <!-- Appointment -->
+  <div v-if="entity === 'appointment'">
+    <div class="q-mb-lg">
+      <div class="show-property-name q-mb-xs">Datum:</div>
+      <div>{{ appointmentDateSpan }}</div>
+    </div>
+    <div class="q-mb-lg">
+      <div class="show-property-name q-mb-xs">Beschreibung:</div>
+      <div>
+        {{ item.description }}
+      </div>
+    </div>
+    <div v-if="item.location" class="q-mb-lg">
+      <div class="show-property-name q-mb-xs">Ort:</div>
+      <div class="row">
+        <div class="col">
+          <div>
+            {{ item.location.name }}
+          </div>
+          <div>
+            {{ item.location.streetAddress }}
+          </div>
+          <div>{{ item.location.postalCode }} {{ item.location.city }}</div>
+        </div>
+        <div class="col">
+          <div class="relative-position full-height">
+            <div class="location-map-button">
+              <q-btn
+                rounded
+                outline
+                color="primary"
+                :label="showMap ? 'Karte schließen' : 'Karte öffnen'"
+                icon="location_on"
+                @click="toggleMapEmbed"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <q-slide-transition>
+        <div v-show="showMap">
+          <GoogleMapsEmbed :location="item.location" class="q-mt-lg" />
+        </div>
+      </q-slide-transition>
+    </div>
+    <div class="q-mb-lg">
+      <div class="show-property-name q-mb-xs">Kontakte:</div>
+      <ContactChip
+        v-for="contact in item.contacts"
+        :key="contact.id"
+        :contact="contact"
+      />
+    </div>
+    <div v-if="item.category" class="q-mb-lg">
+      <div class="show-property-name q-mb-xs">Kategorie:</div>
+      <div>
+        <!-- ToDo: Text color depending on background color -->
+        <q-chip
+          text-color="white"
+          :style="'background-color: ' + item.category.color"
+          >{{ item.category.name }}</q-chip
+        >
+      </div>
+    </div>
+  </div>
+
   <!-- Contact -->
   <div v-if="entity === 'contact'">
     <div class="q-mb-lg">
@@ -42,22 +108,7 @@
       </div>
     </div>
     <div class="q-mb-lg text-center">
-      <!-- <iframe
-        :src="
-          'https://maps.google.com/maps?&amp;q=' +
-          item.streetAddress +
-          ' ' +
-          item.postalCode +
-          ' ' +
-          item.city +
-          '&amp;output=embed'
-        "
-        width="500"
-        height="400"
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-        class="google-maps-iframe"
-      ></iframe> -->
+      <GoogleMapsEmbed :location="item" />
     </div>
   </div>
 
@@ -94,9 +145,18 @@
 
 <script>
 import { defineComponent } from "vue"
+import { date } from "quasar"
+import ContactChip from "./ContactChip.vue"
+import GoogleMapsEmbed from "./GoogleMapsEmbed.vue"
 
 export default defineComponent({
   name: "ItemShow",
+
+  components: {
+    ContactChip,
+    ContactChip,
+    GoogleMapsEmbed,
+  },
 
   props: {
     item: {
@@ -112,6 +172,36 @@ export default defineComponent({
 
   emits: ["deleteClick", "editClick"],
 
+  data() {
+    return {
+      showMap: false,
+    }
+  },
+
+  computed: {
+    appointmentDateSpan() {
+      const jsStartAt = new Date(this.item.startAt)
+      const jsEndAt = new Date(this.item.endAt)
+
+      // Check if start and end is the same day
+      if (jsStartAt.getDate() === jsEndAt.getDate()) {
+        return (
+          date.formatDate(jsStartAt, "DD. MMMM YYYY - HH:mm") +
+          " bis " +
+          date.formatDate(jsEndAt, "HH:mm") +
+          " Uhr"
+        )
+      } else {
+        return (
+          date.formatDate(jsStartAt, "DD. MMMM YYYY - HH:mm") +
+          " Uhr bis " +
+          date.formatDate(jsEndAt, "DD. MMMM YYYY - HH:mm") +
+          " Uhr"
+        )
+      }
+    },
+  },
+
   methods: {
     onDeleteClick() {
       this.$emit("deleteClick", this.item)
@@ -120,14 +210,19 @@ export default defineComponent({
     onEditClick() {
       this.$emit("editClick", this.item)
     },
+
+    toggleMapEmbed() {
+      this.showMap = !this.showMap
+    },
   },
 })
 </script>
 
 <style lang="scss">
-.google-maps-iframe {
-  border: 0;
-  width: 100%;
-  border-radius: 8px;
+.location-map-button {
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translate(0, -50%);
 }
 </style>
