9f5f6d50808f13f508199d1eb19bf6aa3c6aed5a
ToKruAmApp <tobi.k1696@googlemail.com>
2023-09-25 22:04:59 +0200

    Add CategoryChip component

---
+++ frontend/src/components/CategoryChip.vue
@@ -0,0 +1,29 @@
+<!--- ToDo: Integrate CategryChip on all occasions -->
+<template>
+  <q-chip text-color="white" :style="'background-color: ' + category.color">
+    {{ category.name }}
+  </q-chip>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "CategoryChip",
+
+  props: {
+    category: {
+      type: Object,
+      default: null,
+    },
+  },
+
+  data() {
+    return {}
+  },
+
+  methods: {},
+})
+</script>
+
+<style lang="scss"></style>
