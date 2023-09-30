139a5381b29063dde28aaff1b2a55f805936d39b
ToKruAmApp <tobi.k1696@googlemail.com>
2023-09-30 17:21:28 +0200

    Add more searchable properties to appoinment list

--- backend/models/Appointment.php
+++ backend/models/Appointment.php
@@ -70,7 +70,7 @@
                     // ToDo: Look at this beauty
                     $query = 'SELECT appointments.id, appointments.name AS appointmentName, appointments.description AS appointmentDescription, startAt, endAt, icon, categories.id AS categoryID, categories.name AS categoryName, categories.description AS categoryDescription, color, locations.id AS locationID, locations.name AS locationName, locations.description AS locationDescription, street_address AS streetAddress, postal_code AS postalCode, city
                     FROM appointments LEFT JOIN categories ON appointments.category = categories.id LEFT JOIN locations ON appointments.location = locations.id
-                    WHERE appointments.startAt > ? AND (appointments.name LIKE ? OR appointments.description LIKE ?)  -- ToDo: Add more properties
+                    WHERE appointments.startAt > ? AND (appointments.name LIKE ? OR appointments.description LIKE ? OR categories.name LIKE ? OR locations.name LIKE ?)
                     ORDER BY startAt ASC
                     LIMIT ?, ?';
                     $stmt = $this->conn->prepare($query);
@@ -78,8 +78,10 @@
                     $stmt->bindParam(1, $this->timespanStart, PDO::PARAM_STR);
                     $stmt->bindParam(2, $searchQuery, PDO::PARAM_STR);
                     $stmt->bindParam(3, $searchQuery, PDO::PARAM_STR);
-                    $stmt->bindParam(4, $this->offset, PDO::PARAM_INT);
-                    $stmt->bindParam(5, $this->limit, PDO::PARAM_INT);
+                    $stmt->bindParam(4, $searchQuery, PDO::PARAM_STR);
+                    $stmt->bindParam(5, $searchQuery, PDO::PARAM_STR);
+                    $stmt->bindParam(6, $this->offset, PDO::PARAM_INT);
+                    $stmt->bindParam(7, $this->limit, PDO::PARAM_INT);
                     $stmt->execute();
                 }
             }
@@ -104,13 +106,15 @@
             } else {
 
                 $query = 'SELECT COUNT(*) AS total 
-                FROM appointments
-                WHERE appointments.startAt > ? AND (appointments.name LIKE ? OR appointments.description LIKE ?)'; // ToDo: Add more properties
+                FROM appointments LEFT JOIN categories ON appointments.category = categories.id LEFT JOIN locations ON appointments.location = locations.id
+                WHERE appointments.startAt > ? AND (appointments.name LIKE ? OR appointments.description LIKE ? OR categories.name LIKE ? OR locations.name LIKE ?)';
                 $stmt = $this->conn->prepare($query);
                 $searchQuery = "%".$this->searchQuery."%";
                 $stmt->bindParam(1, $this->timespanStart, PDO::PARAM_STR);
                 $stmt->bindParam(2, $searchQuery, PDO::PARAM_STR);
                 $stmt->bindParam(3, $searchQuery, PDO::PARAM_STR);
+                $stmt->bindParam(4, $searchQuery, PDO::PARAM_STR);
+                $stmt->bindParam(5, $searchQuery, PDO::PARAM_STR);
                 $stmt->execute();
             }
 
