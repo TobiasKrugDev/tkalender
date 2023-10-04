add27c91c3babe63933808461149e5df4f50dc45
ToKruAmApp <tobi.k1696@googlemail.com>
2023-10-04 14:53:56 +0200

    Add own login layout

---
+++ frontend/src/layouts/LoginLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="lHh lpR lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "LoginLayout",
+
+  components: {},
+
+  data() {
+    return {}
+  },
+
+  methods: {},
+})
+</script>
+
+<style lang="scss"></style>
