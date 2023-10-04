0d9d36b249f5485e9bd7a338adb932c5280e497d
ToKruAmApp <tobi.k1696@googlemail.com>
2023-10-04 15:39:26 +0200

    Add navigation guard with auth check

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,7 @@ import {
   createWebHashHistory,
 } from "vue-router"
 import routes from "./routes"
+import { Cookies } from "quasar"
 
 /*
  * If not building with SSR mode, you can
@@ -31,6 +32,28 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE),
+  })
+
+  Router.beforeEach((to, from, next) => {
+    const loggedIn = Cookies.has("PHPSESSID") // Check for cookie set by PHP session
+
+    // User wants to access /login
+    if (to.path === "/login") {
+      // Redirect to "/" if user is already logged in
+      if (loggedIn) {
+        next("/")
+      } else {
+        next()
+      }
+      // User wants to access anything else than /login
+    } else {
+      if (loggedIn) {
+        next()
+      } else {
+        // Block access when user isn't authenticated -> redirect to /login
+        next("/login")
+      }
+    }
   })
 
   return Router
