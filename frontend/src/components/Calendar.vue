50480417521f7a22d370c071b89cdd9447b4c9f8
ToKruAmApp <tobi.k1696@googlemail.com>
2023-11-05 21:16:34 +0100

    Include appointments of next month in calendar fetch request

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -339,7 +339,7 @@ export default defineComponent({
       )
       const end = new Date(
         e.startDate.getFullYear(),
-        e.startDate.getMonth() + 1
+        e.startDate.getMonth() + 2
       )
 
       this.timespanStart = start
