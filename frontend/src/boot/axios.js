c59712fb9611c69df2e72d4d6af27db5e282fba2
TobiasKrugDev <tobi.k1696@googlemail.com>
2024-03-23 20:52:08 +0100

    Reinclude axios api variable to prevent errors

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -7,9 +7,9 @@ import axios from "axios"
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-// const api = axios.create({
-//   baseURL: "https://www.tkalender-demo.de/api",
-// })
+const api = axios.create({
+  baseURL: "https://www.tkalender-demo.de/api",
+})
 
 axios.defaults.baseURL = "https://www.tkalender-demo.de"
 
@@ -20,9 +20,9 @@ export default boot(({ app }) => {
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
-  // app.config.globalProperties.$api = api
+  app.config.globalProperties.$api = api
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
 })
 
-export { axios }
+export { axios, api }
