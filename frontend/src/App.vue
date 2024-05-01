31bfb02fb700992228f5970a22fbb1454e167c11
TobiasKrugDev <tobi.k1696@googlemail.com>
2024-05-01 22:56:29 +0200

    Set primary color as background for status bar in native app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,5 +15,12 @@ import { defineComponent } from "vue"
 
 export default defineComponent({
   name: "App",
+
+  mounted () {
+    // In the native mobile app set primary color as background for status bar
+    if (this.$q.platform.is.capacitor) {
+      this.$q.capacitor.Plugins.StatusBar.setBackgroundColor({ color: "#2121ce" }) // ToDo: check if primary variable can be used here
+    }
+  }
 })
 </script>
