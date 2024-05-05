64819138618a67740ae01563bbf1eab4cea455dc
TobiasKrugDev <tobi.k1696@googlemail.com>
2024-05-05 20:14:58 +0200

    Validate JWT token on server requests

--- backend/helpers/auth_check.php
+++ backend/helpers/auth_check.php
@@ -1,14 +1,28 @@
 <?php
-    function checkAuthentication($sessionID) {
-        session_start();
-        $sessionExists = file_exists(session_save_path().'/sess_'.$sessionID);
-        // Check if user session is active
-        if (!isset($_SESSION["user_id"]) && !$sessionExists) {
-            // User is not logged in --> return 401 status code with error message and exit script
-            http_response_code(401);
-            echo json_encode(
-                array('message' => 'Access not granted')
-            );
-            exit;
+    include_once '../../vendor/autoload.php';
+    include_once '../../config/jwt_secret.php';
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
+
+    function checkAuthentication() {
+        $token = $_GET['token'];
+        
+        if (!$token) {
+            denyAccess();
         }
+
+        // ToDo: Add check for token expiration
+        try {
+            JWT::decode($token, new Key($GLOBALS['jwt_secret'], 'HS256'));
+        } catch (Exception $e) {
+            denyAccess();
+        }
+    }
+
+    function denyAccess() {
+        http_response_code(401);
+        echo json_encode(
+            array('message' => 'Access not granted')
+        );
+        exit;
     }
